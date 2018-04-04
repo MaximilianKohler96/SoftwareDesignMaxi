34162595a7ec2b32badbb0f038ca14f2e6f9ef0a
MaximilianKohler96 <37618445+MaximilianKohler96@users.noreply.github.com>
2018-04-04 11:18:39 +0200

    YO

---
+++ Neuer Ordner/Übung 1b/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Übung_1b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
