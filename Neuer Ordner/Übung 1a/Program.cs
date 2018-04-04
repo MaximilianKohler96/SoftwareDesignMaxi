f5cbafc3f71f6a93152042a1828fdd7818b883db
MaximilianKohler96 <37618445+MaximilianKohler96@users.noreply.github.com>
2018-04-04 11:20:46 +0200

    yo1

--- Neuer Ordner/Übung 1a/Program.cs
+++ Neuer Ordner/Übung 1a/Program.cs
@@ -4,66 +4,9 @@ namespace Übung_1a
 {
     class Program
     {
-      
         static void Main(string[] args)
         {
-            
-           Console.WriteLine("Für Würfel -> w , Für Kugel -> k , Für Oktaeder -> o");
-           string klasse = Console.ReadLine();
-
-           Console.WriteLine("Der Durchmesser d beträgt?");
-           double d = Console.Read();
-
-           if (klasse == "w" ) 
-            {
-                Console.WriteLine("Die Oberfläche beträgt: " + getCubeOberflaeche(d));
-                Console.WriteLine("Das Volumen beträgt: " + getCubeVolumen(d));
-            }
-
-            if (klasse == "k")
-            {
-                Console.WriteLine("Die Oberfläche beträgt: " + getSphereOberflaeche(d));
-                Console.WriteLine("Das Volumen beträgt: " + getSphereVolumen(d));
-            }
-            
-            if (klasse == "o"){
-                Console.WriteLine("Die Oberfläche beträgt: " + getOktaOberflaeche(d));
-                Console.WriteLine("Das Volumen beträgt: " + getOktaVolumen(d));
-            }
-                  
-                               
+            Console.WriteLine("Hello World!");
         }
-
-        public static double getCubeOberflaeche(double d) {
-            double oberflaeche = 6*(d*d);
-            return oberflaeche;
-        }
-
-        public static double getCubeVolumen(double d) {
-            double volumen = d*d*d;
-            return volumen;
-        }
-
-         public static double getSphereOberflaeche(double d) {
-            double oberflaeche = d*d*3.14;
-            return oberflaeche;
-        }
-
-        public static double getSphereVolumen(double d) {
-            double volumen = ((d*d*d)*3.14)/6;
-            return volumen;
-        }
-
-        public static double getOktaOberflaeche(double d) {
-            double oberfläche = 2 * Math.Sqrt(3) * (d * d);
-            return oberfläche;
-        }
-
-        public static double getOktaVolumen(double d) {
-            double volumen = (Math.Sqrt(2) * (d * d * d)) / 3;
-            return volumen;
-        }
-
-
     }
 }
