1e0dde502543e1da8d4da35b9adf7af420a5a05d
MaximilianKohler96 <37618445+MaximilianKohler96@users.noreply.github.com>
2018-04-07 15:15:38 +0200

    yoyo

--- Neuer Ordner/Übung 1b/Program.cs
+++ Neuer Ordner/Übung 1b/Program.cs
@@ -18,9 +18,34 @@ namespace Aufgabe1._2
 
         for (int i=4; i > -1; i-- ) {
             Random rnd = new Random();
-            int rsub = rnd.Next(0, i+1)
-            int rver =
-            int robj =
+            int rsub = rnd.Next(0, i+1);
+            int rver = rnd.Next(0, i+1);
+            int robj = rnd.Next(0, i+1);
+
+            subject = subjects[rsub];
+            verb = verbs[rver];
+            objectab = objects[robj];
+
+            int hsub = i - rsub;
+            int hver = i - rver;
+            int hobj = i - robj;
+
+            for (int z = hsub; z > 0; z=z - 1 ) {
+                subjects [rsub] = subjects [rsub + 1];
+                rsub = rsub + 1;
+            }
+
+            for (int z = hver; z > 0; z=z - 1) {
+                verbs [rver] = verbs [rver + 1];
+                rver++;
+            }
+
+            for (int z = hobj; z > 0; z=z - 1) {
+                objects[robj] = objects[robj + 1];
+                robj++;
+            }
+
+            Console.WriteLine(subject + " " + verb + " " + objectab);
 
         }
 
