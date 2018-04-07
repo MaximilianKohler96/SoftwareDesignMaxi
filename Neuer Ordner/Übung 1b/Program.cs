1af264e0322147582e231bb22b5c5d43ed03a43a
MaximilianKohler96 <37618445+MaximilianKohler96@users.noreply.github.com>
2018-04-07 15:38:41 +0200

    yo

--- Neuer Ordner/Übung 1b/Program.cs
+++ Neuer Ordner/Übung 1b/Program.cs
@@ -5,7 +5,6 @@ namespace Übung_1b
     class Program
     {
         static void Main(string[] args) {
-
         
             string[] subjects = { "Gandalf", "Frodo", "Boromir", "Sauron", "Galadriel", "Aragorn" };
             string[] verbs = { "trinkt", "braut", "hasst", "zaubert", "schneidet", "beschwört" };
@@ -15,44 +14,44 @@ namespace Übung_1b
         }
     
 
-    public static void getVerse(string[] subjects, string[] verbs, string[] objects) {
-        string verb;
-        string subject;
-        string objectab;
+        public static void getVerse(string[] subjects, string[] verbs, string[] objects) {
+            string verb;
+            string subject;
+            string objectab;
 
-        for (int i=4; i > -1; i-- ) {
-            Random rnd = new Random();
-            int rsub = rnd.Next(0, i+1);
-            int rver = rnd.Next(0, i+1);
-            int robj = rnd.Next(0, i+1);
+            for (int i=4; i > -1; i-- ) {
+                Random rnd = new Random();
+                int rsub = rnd.Next(0, i+1);
+                int rver = rnd.Next(0, i+1);
+                int robj = rnd.Next(0, i+1);
 
-            subject = subjects[rsub];
-            verb = verbs[rver];
-            objectab = objects[robj];
+                subject = subjects[rsub];
+                verb = verbs[rver];
+                objectab = objects[robj];
 
-            int hsub = i - rsub;
-            int hver = i - rver;
-            int hobj = i - robj;
+                int hsub = i - rsub;
+                int hver = i - rver;
+                int hobj = i - robj;
 
-            for (int z = hsub; z > 0; z=z - 1 ) {
-                subjects [rsub] = subjects [rsub + 1];
-                rsub = rsub + 1;
+                for (int z = hsub; z > 0; z=z - 1 ) {
+                    subjects [rsub] = subjects [rsub + 1];
+                    rsub = rsub + 1;
+                }
+
+                for (int z = hver; z > 0; z=z - 1) {
+                    verbs [rver] = verbs [rver + 1];
+                    rver++;
+                }
+
+                for (int z = hobj; z > 0; z=z - 1) {
+                    objects[robj] = objects[robj + 1];
+                    robj++;
+                }
+
+                Console.WriteLine(subject + " " + verb + " " + objectab);
             }
-
-            for (int z = hver; z > 0; z=z - 1) {
-                verbs [rver] = verbs [rver + 1];
-                rver++;
-            }
-
-            for (int z = hobj; z > 0; z=z - 1) {
-                objects[robj] = objects[robj + 1];
-                robj++;
-            }
-
-            Console.WriteLine(subject + " " + verb + " " + objectab);
 
         }
 
     }
-}
 }
