fa1a0d7349b9bd27f419a186c3c339dcdc268884
MaximilianKohler96 <37618445+MaximilianKohler96@users.noreply.github.com>
2018-04-10 11:51:20 +0200

    yoyoyo

---
+++ Neuer Ordner/Übung VL/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Übung_VL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person einePerson = new Person();
+            einePerson.FirstName = "Horst";
+            einePerson.Age = 42;
+
+            Console.WriteLine()
+
+        }
+
+        public class Person
+        {
+            public string FirstName;
+            public int Age;
+        }
+    }
+}
