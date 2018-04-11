a28caf20d8a6dd0ea114687e91bb396d0a4c7a90
MaximilianKohler96 <37618445+MaximilianKohler96@users.noreply.github.com>
2018-04-11 12:09:58 +0200

    yo

--- Neuer Ordner/Übung 2a/Program.cs
+++ Neuer Ordner/Übung 2a/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using static System.Console;
 
-namespace Übung_2a
+public class SimplePerson
+{
+   public string FirstName;
+   public string LastName;
+   public DateTime DateOfBirth;
+}
+
+namespace Debugging
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person root = Familytree.BuildTree();
+
+            Person found = Familytree.Find(root);
+
+            WriteLine(found);
         }
     }
 }
