2bdaa7e98649c8422474e9799a22c7fd9145a027
MaximilianKohler96 <37618445+MaximilianKohler96@users.noreply.github.com>
2018-04-21 15:25:23 +0200

    yp

--- Neuer Ordner/Übung 3/Program.cs
+++ Neuer Ordner/Übung 3/Program.cs
@@ -6,7 +6,24 @@ namespace Übung_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int value = 432;
+            Console.WriteLine(toHexal(value));
         }
+
+        static int toHexal(int value)
+        {
+            int result = 0;
+            int u = value.ToString().Length;
+            for (int i = 0; i < u; i++)
+            {
+                double potency = Math.Pow(6, Convert.ToDouble(i));
+                int rest = (value % 10) * Convert.ToInst32(potency);
+                result = rest + result;
+                value = value / 10;
+            }
+            return result;
+        }
+
     }
 }
+
