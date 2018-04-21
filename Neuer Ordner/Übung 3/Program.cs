2d210b888c28ac884cac01e49604ade6028d267f
MaximilianKohler96 <37618445+MaximilianKohler96@users.noreply.github.com>
2018-04-21 17:30:57 +0200

    yo

--- Neuer Ordner/Übung 3/Program.cs
+++ Neuer Ordner/Übung 3/Program.cs
@@ -6,24 +6,62 @@ namespace Übung_3
     {
         static void Main(string[] args)
         {
-            int value = 432;
-            Console.WriteLine(toHexal(value));
+            Console.WriteLine("Für Hexalzahl in Dezimalzahl umwandeln:  HtoD     Dezimal in Hexalzahl umwandeln:  DtoH   ");
+            string klasse = Console.ReadLine();
+
+            Console.WriteLine("Wie lautet deine Zahl?");
+            double input = double.Parse(Console.ReadLine());
+
+            int output = Convert.ToInt32(input);
+                     
+
+            int hex = output;
+            int dec = output;
+
+            if (klasse == "HtoD" )
+            {
+                Console.WriteLine("Die Hexalzahl " + hex + " wird zur Dezimalzahl " + ConvertDecimaltoHexal(hex));
+            } 
+            
+            if (klasse == "DtoH" )
+            {
+                Console.WriteLine("Die Dezimalzahl " + dec + " wird zur Hexalzahl " + ConvertHexaltoDecimal(dec));
+            }
+
+                                  
         }
 
-        static int toHexal(int value)
+        static int ConvertDecimaltoHexal(int hex)
         {
             int result = 0;
-            int u = value.ToString().Length;
+            int u = hex.ToString().Length;
             for (int i = 0; i < u; i++)
             {
                 double potency = Math.Pow(6, Convert.ToDouble(i));
-                int rest = (value % 10) * Convert.ToInst32(potency);
+                int rest = (hex % 10) * Convert.ToInt32(potency);
                 result = rest + result;
-                value = value / 10;
+                hex = hex / 10;
             }
             return result;
         }
 
+        static int ConvertHexaltoDecimal(int dec)
+        {
+            int result = 0;
+            int u = dec.ToString().Length;
+            for (int i = 0; i < u; i++)
+            {
+                double potency = Math.Pow(10, Convert.ToDouble(i));
+                int rest = (dec % 6) * Convert.ToInt32(potency);
+                result = rest + result;
+                dec = dec / 6;
+            }
+            return result;
+
+        }
+
+        //static int ConvertToBaseFromDecimal(int toBase, int dec)
+        
     }
 }
 
