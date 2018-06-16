9f73d72168498099b8d2c83b401b14d2097cbd92
MaximilianKohler96 <37618445+MaximilianKohler96@users.noreply.github.com>
2018-06-16 12:08:55 +0200

    ol

---
+++ Neuer Ordner/Tic_Tac_Toe/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tic_Tac_Toe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char[,] matchfield = new char[3, 3];
+            matchfield[0, 0] = 'X';
+            matchfield[2, 2] = 'O';
+
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    Console.WriteLine("|    |  X  |    |");
+                }
+                Console.WriteLine();
+            }
+
+            
+        }
+    }
+}
---
+++ Neuer Ordner/Unit-Test/Mail_Checker/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Mail_Checker
+{
+    class Program
+    {
+        public static bool IsEmailAddress(string emailAdress)
+        {
+            int iAt = emailAdress.IndexOf('@');
+            int iDot = emailAdress.LastIndexOf('.');
+            return (iAt > 0 && iDot > iAt);
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsEmailAddress("irgendwas@yahoo.de"));
+
+            Console.WriteLine(IsEmailAddress("@yahoo.de"));
+
+            Console.WriteLine(IsEmailAddress("hallo.kohler@yahoo.de"));
+        }
+    }
+}
